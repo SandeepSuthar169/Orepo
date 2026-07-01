d001066ec95b6d07e5bbe5d8b35369133ea6a5ba
Sandeep Suthar <ss.suthar.004@gmail.com>
2026-07-01 15:29:53 +0530

    Add: Creating hero section

--- src/page/HomePage.tsx
+++ src/page/HomePage.tsx
@@ -8,9 +8,9 @@ const HomePage = () => {
         <main className="flex">
           <section className="mx-auto lg:h-281.75  md:h-250 min-h-screen w-full h-full">
             <img 
-              src="src\assets\hero.png" 
+              src="src\assets\hero.webp" 
               alt=""  
-              className="absolute inset-0 h-full w-full object-cover"  
+              className="absolute  inset-0 h-full w-full object-cover mask-b-from-50%"  
             />
 
             <div className="relative flex  min-h-dvh w-full ">
@@ -21,34 +21,37 @@ const HomePage = () => {
                           Here are 1700+ Pojects <br />
                       </h1>
                       <h1 className="text-5xl  font-bold text-white/70 ">
-                      you can <span className="text-rose-500">contribure</span>   
+                        you can <span className="text-rose-500">contribure</span>   
                       </h1>
                   </div>
 
                   <div className="flex flex-col items-center gap-2 mb-1">
-                  <p className="text-base md:text-2xl px-5 md:px-10 font-normal text-white tracking-normal opacity-80 max-w-2xl">
-                    Search what you need  and let Ople handle the rest.
-                  </p>
-                  <p className="text-base md:text-2xl px-0 md:px-0 font-normal text-shadow-gray-600 text-white tracking-normal opacity-80 max-w-2xl">
-                    Contribute in AI, Frontend, DataBase, npm. <span className="text-black  font-bold"> You are needed. </span>
-                  </p>
+                    <p className="text-base md:text-2xl px-5 md:px-10 font-normal text-white tracking-normal opacity-80 max-w-2xl">
+                      Search what you need  and let Ople handle the rest.
+                    </p>
+                    <p className="text-base md:text-2xl px-0 md:px-0 font-normal text-shadow-gray-600 text-white tracking-normal opacity-80 max-w-2xl">
+                      Contribute in AI, Frontend, DataBase, npm. <span className="text-black  font-bold"> You are needed. </span>
+                    </p>
                   </div>
                   <div className=" flex items-center gap-6  font-medium">
-                <button className="h-12 px-8  rounded-full bg-linear-to-b from-white to-gray-200 text-black font-semibold shadow-[0_4px_20px_rgba(59,130,246,0.4)] hover:shadow-[0_4px_25px_rgba(59,130,246,0.6)] hover:scale-105 transition-all duration-300">
+                     <button className="h-12 px-8  rounded-full bg-linear-to-b from-white to-gray-200 text-black font-semibold shadow-[0_4px_20px_rgba(59,130,246,0.4)] hover:shadow-[0_4px_25px_rgba(59,130,246,0.6)] hover:scale-105 transition-all duration-300">
                         Get Started
                     </button>
                     <button className="h-12 px-8 rounded-full border border-gray-700 bg-gray-900/50 text-white font-medium hover:bg-gray-800 transition-colors backdrop-blur-sm flex items-center gap-2 group">
-                    Source
+                        Source
                         <FiArrowUpRight className="w-4 h-4 fill-white group-hover:scale-110 transition-transform" />
                     </button>
+                  </div>
                 </div>
-                  
-                </div>
-           
-
               </div>
-
             </div>
+            <div className="relative mx-auto max-w-4xl rounded-3xl border border-neutral-200/50 bg-neutral-100 p-2 backdrop-blur-lg dark:border-neutral-600 dark:bg-neutral-700/50 md:p-4">
+              <div className="rounded-3xl border border-neutral-200 bg-white p-2 dark:border-neutral-700 dark:bg-black">
+                <img src="src\assets\dashboard-new.webp" alt="" className="rounded-[20px]" />
+              </div>
+            </div>
+            
+
 
           </section>
         </main>
