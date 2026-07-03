1298cf5da8b599be7e5137cb9e0167d3f2cc101d
Sandeep Suthar <ss.suthar.004@gmail.com>
2026-07-03 19:08:56 +0530

    Add: Added files

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { Route, Routes  } from "react-router-dom"
 import HomePage from "./page/HomePage"
 import Layout from "./layout/Layout"
+import DataPage from "./page/DataPage"
 
 const App = () => {
   return (
@@ -11,6 +12,10 @@ const App = () => {
             <Route 
               path="/"
               element={<HomePage />}
+            />
+            <Route 
+              path="/data"
+              element={<DataPage />}
             />
           </Route>
         </Routes>
